5711a2d84173b9cce2c6806f42696b01c766b8ad
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-11-01 15:35:42 +0200

    first version of list of todos page

--- src/main/java/com/mohamed/springboot/todo/TodoService.java
+++ src/main/java/com/mohamed/springboot/todo/TodoService.java
@@ -1,18 +1,22 @@
 package com.mohamed.springboot.todo;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TodoService {
 	
-	private static List<Todo> todos;
+	private static List<Todo> todos = new ArrayList<Todo>();
 	
 	static {
 		todos.add(new Todo(1, "Mohamed", "Learn Spring Boot",
 				LocalDate.now().plusYears(1), false));
-		todos.add(new Todo(1, "Mohamed", "Learn AWS",
+		todos.add(new Todo(2, "Mohamed", "Learn AWS",
 				LocalDate.now().plusYears(1), false));
-		todos.add(new Todo(1, "Mohamed", "Learn Git Version Control",
+		todos.add(new Todo(3, "Mohamed", "Learn Git Version Control",
 				LocalDate.now().plusYears(1), false));
 	}
 	
