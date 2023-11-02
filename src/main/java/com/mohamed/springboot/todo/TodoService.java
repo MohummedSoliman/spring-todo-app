a1897bbfa000070cc891ba2bec0d0cc288ee4ea4
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-11-02 13:39:28 +0200

    add method for searching particular todo using id

--- src/main/java/com/mohamed/springboot/todo/TodoService.java
+++ src/main/java/com/mohamed/springboot/todo/TodoService.java
@@ -10,32 +10,34 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
-	
+
 	private static List<Todo> todos = new ArrayList<Todo>();
 	private static int todosCount = 0;
-	
+
 	static {
-		todos.add(new Todo(++todosCount, "Mohamed", "Learn Spring Boot",
-				LocalDate.now().plusYears(1), false));
-		todos.add(new Todo(++todosCount, "Mohamed", "Learn AWS",
-				LocalDate.now().plusYears(3), false));
-		todos.add(new Todo(++todosCount, "Mohamed", "Learn Git Version Control",
-				LocalDate.now().plusYears(4), false));
+		todos.add(new Todo(++todosCount, "Mohamed", "Learn Spring Boot", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(++todosCount, "Mohamed", "Learn AWS", LocalDate.now().plusYears(3), false));
+		todos.add(new Todo(++todosCount, "Mohamed", "Learn Git Version Control", LocalDate.now().plusYears(4), false));
 	}
-	
-	public List<Todo> findByUsername(String username){
+
+	public List<Todo> findByUsername(String username) {
 		return todos;
 	}
-	
-	public void addTodo(String name, String description,
-						LocalDate targetDate,boolean done) {
-		todos.add(new Todo(++todosCount, name, description,
-				targetDate, done));
+
+	public void addTodo(String name, String description, LocalDate targetDate, boolean done) {
+		todos.add(new Todo(++todosCount, name, description, targetDate, done));
 	}
-	
+
 	public void deleteTodo(int id) {
-		
+
 		Predicate<? super Todo> prdicate = todo -> todo.getId() == id;
 		todos.removeIf(prdicate);
+	}
+
+	public Todo findById(int id) {
+		return todos.stream().
+					filter(todo -> todo.getId() == id).
+					findFirst().
+					get();
 	}
 }
