ff8b6f94593844af58831e35a7d360c723470cb4
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-11-08 16:17:11 +0200

    add new bean for enable access from another origin

--- src/main/java/com/mohamed/springboot/todo/TodoResource.java
+++ src/main/java/com/mohamed/springboot/todo/TodoResource.java
@@ -3,11 +3,13 @@ package com.mohamed.springboot.todo;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class TodoResource {
 	
 	private TodoService todoService;
