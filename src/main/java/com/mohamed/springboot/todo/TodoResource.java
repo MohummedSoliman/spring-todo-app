87b48a05a70aaebd4b2f2c76aae9dd96e42ea2c3
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-11-09 12:11:55 +0200

    add updateTodo and add new Todo rest API

--- src/main/java/com/mohamed/springboot/todo/TodoResource.java
+++ src/main/java/com/mohamed/springboot/todo/TodoResource.java
@@ -1,19 +1,24 @@
 package com.mohamed.springboot.todo;
 
-
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000/")
 public class TodoResource {
-	
+
 	private TodoService todoService;
-	
+
 	public TodoResource(TodoService todoService) {
 		this.todoService = todoService;
 	}
@@ -22,18 +27,36 @@ public class TodoResource {
 	public List<Todo> retrieveTodos(@PathVariable String username) {
 		return todoService.findByUsername(username);
 	}
-	
+
 	@GetMapping("users/{username}/todos/{id}")
 	public Todo retrieveTodo(@PathVariable String username, @PathVariable int id) {
 		return todoService.findById(id);
 	}
-	
+
 	@DeleteMapping("users/{username}/todos/{id}")
-	public ResponseEntity<Object>deleteTodo(@PathVariable String username, @PathVariable int id) {
+	public ResponseEntity<Object> deleteTodo(@PathVariable String username, @PathVariable int id) {
 		todoService.deleteTodo(id);
 		return ResponseEntity.noContent().build();
 	}
+
+	@PutMapping("/users/{username}/todos/{id}")
+	public Todo updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody Todo todo) {
+		todoService.updateTodo(todo);
+		return todo;
+	}
+
+	@PostMapping("/users/{username}/todos")
+	public ResponseEntity<Void> createTodo(@PathVariable String username, @RequestBody Todo todo) {
+		todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), false);
+		URI location = URI.create("http://localhost:3000/users/"+username+"/todos/"+todo.getId());
+		return ResponseEntity.created(location).build();
+	}
 }
+
+
+
+
+
 
 
 
